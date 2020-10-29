a6ba86441a08bb9143a0f6648e8c6884d57f2f6d
Shri Lekkala <shri.lekkala18@imperial.ac.uk>
2020-10-30 00:33:41 +0530

    Completed and Tested Ex 3.7, Ex 3.8

--- cla_utils/exercises4.py
+++ cla_utils/exercises4.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from numpy import random
 
 def operator_2_norm(A):
     """
@@ -10,10 +10,26 @@ def operator_2_norm(A):
     :return o2norm: the norm
     """
 
-    raise NotImplementedError
+    # compute the largest eigenvalue of A^* A
+    lambda_max = max(np.linalg.eigvals(A.conjugate().T @ A))
+
+    # the operator norm of A is the square root of lambda_max
+    o2norm = np.sqrt(lambda_max)
 
     return o2norm
 
+def verify_ineq(m, n):
+    A = random.randn(m, n)
+    x = random.randn(n, 1)
+
+    # check if the inequality is satisfied
+    if operator_2_norm(A@x) <= operator_2_norm(A) * operator_2_norm(x):
+        print("Inequality satisfied")
+    else:
+        print("Inequality not satisfied")
+
+# check if the inequality is satisfied for particular m, n
+verify_ineq(20, 7)
 
 def cond(A):
     """
