7f80709f3711ba93b9788af55b2a760b3a5a0b0b
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-29 07:06:59 +0530

    Updated Ex 4.13

--- cla_utils/exercises7.py
+++ cla_utils/exercises7.py
@@ -142,20 +142,23 @@ def det_LUP(A):
 
     return detA
 
-def ex4_13():
-    I = np.eye(10)
-    e3 = I[:,2]
-    e9 = I[:,8]
-    A = I + np.outer(e3, e9.conjugate())
-    
+def ex4_13(m):
+    A0 = 3 * np.eye(m)
+    A0[0, :] = np.ones(m)
+    A0[:, 0] = np.arange(1, m+1)
+    A = A0 * 1.0
+
     # In place LU decomposition without pivoting
     for k in range(m-1):
         A[k+1:, k] = A[k+1:, k] / A[k, k]
         A[k+1:, k+1:] = A[k+1:, k+1:] - np.outer(A[k+1:, k], A[k, k+1:])
 
-    i1 = np.tril_indices(10, k=-1)
-    L = np.eye(10)
+    i1 = np.tril_indices(m, k=-1)
+    L = np.eye(m)
     L[i1] = A[i1]
     U = np.triu(A)
 
-    return A,L,U
+    print("Number of non-zero entries in L: " + str(np.count_nonzero(L)))
+    print("Number of non-zero entries in U: " + str(np.count_nonzero(U)))
+
+    return L, U
