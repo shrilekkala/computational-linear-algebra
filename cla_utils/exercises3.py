792130d1d3edd445a835b06ac60b9ad8ab2f9bae
Shri Lekkala <shri.lekkala@gmail.com>
2020-12-07 00:13:46 +0530

    Added tests for complex householder functions

--- cla_utils/exercises3.py
+++ cla_utils/exercises3.py
@@ -7,10 +7,10 @@ def householder(A, kmax=None):
     using Householder transformations.
 
     :param A: an mxn-dimensional numpy array
-    :param kmax: an integer, the number of columns of A to reduce \
+    :param kmax: an integer, the number of columns of A to reduce
     to upper triangular. If not present, will default to n.
 
-    :return R: an mxn-dimensional numpy array containing the upper \
+    :return R: an mxn-dimensional numpy array containing the upper
     triangular matrix
     """
 
@@ -149,3 +149,77 @@ def householder_ls(A, b):
     x = scipy.linalg.solve_triangular(R[:, :m], Q_star_b)
 
     return x
+
+# sign function for use in complex Householder algorithm
+def sign(x):
+    if x == 0:
+        s = 1
+    else:
+        s = x/abs(x)
+    return(s)
+
+def householder_complex(A, kmax=None):
+    """
+    Given a complex mxn matrix A, find the reduction to upper triangular matrix R
+    using Householder transformations.
+
+    :param A: an mxn-dimensional numpy array
+    :param kmax: an integer, the number of columns of A to reduce
+    to upper triangular. If not present, will default to n.
+
+    :return R: an mxn-dimensional numpy array containing the upper
+    triangular matrix
+    """
+    #A = Ac.copy(dtype = 'complex')
+    m, n = A.shape
+
+    if kmax is None:
+        kmax = n
+
+    # k cycles from 1 to n
+    for k in range(1, kmax+1):
+
+        x = A[k-1: m+1, k-1]
+
+        # initialise e_1 of length m-k+1
+        e_1 = np.eye(np.size(x,0))[:,0]
+
+        # householder algorithm
+        v_k = sign(x[0]) * np.linalg.norm(x) * e_1 + x
+        v_k = v_k / np.linalg.norm(v_k)
+
+        A[k-1: m+1, k-1: kmax] = A[k-1: m+1, k-1: kmax]  - 2 * np.outer(v_k, v_k.conjugate()) @ A[k-1: m+1, k-1: kmax]
+
+        # for case when kmax =/= n, compute Q*b in place of b
+        A[:, kmax:][k-1:,:] = A[:, kmax:][k-1:,:] - 2 * np.outer(v_k, v_k.conjugate()) @ A[:, kmax:][k-1:,:]
+    return A
+
+def householder_qr_complex(A):
+    """
+    Given a complex mxn matrix A, use the Householder transformation to find
+    the reduced QR factorisation of A.
+
+    :param A: an mxn-dimensional numpy array
+
+    :return Q: an mxn-dimensional numpy array
+    :return R: an nxn-dimensional numpy array
+    """
+    #A = Ac.copy(dtype = 'complex')
+    m, n = A.shape
+    
+    # create an mxm identity matrix
+    I = np.eye(m)
+
+    # construct extended array Ahat
+    A_hat = np.hstack((A, I))
+
+    # use housholder to computer Q and R
+    A_hat_householder = householder_complex(A_hat, n)
+
+    # extract Q_star and R using slice notation
+    R = A_hat_householder[:n, :n]
+    Q_star = A_hat_householder[:, n:]
+
+    # construct Q by transposing Q_star and taking the first n columns
+    Q = Q_star.conjugate().T[:, :n]
+    return Q, R
