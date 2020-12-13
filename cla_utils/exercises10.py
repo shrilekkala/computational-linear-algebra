74cc26569e36e381601b6c70a775bf47df7a3bae
Shri Lekkala <shri.lekkala@gmail.com>
2020-12-14 01:09:04 +0530

    Updated Ex 6.2 to form Q and R from least squares explicitly

--- cla_utils/exercises10.py
+++ cla_utils/exercises10.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.random as random
-from cla_utils.exercises3 import householder_ls
+import scipy.linalg
+# from cla_utils.exercises3 import householder_ls
 
 def arnoldi(A, b, k):
     """
@@ -33,6 +34,111 @@ def arnoldi(A, b, k):
     
     return Q, H
 
+def householder(A, kmax=None):
+    """
+    Given a real mxn matrix A, find the reduction to upper triangular matrix R
+    using Householder transformations.
+
+    :param A: an mxn-dimensional numpy array
+    :param kmax: an integer, the number of columns of A to reduce
+    to upper triangular. If not present, will default to n.
+
+    :return R: an mxn-dimensional numpy array containing the upper
+    triangular matrix
+    """
+
+    m, n = A.shape
+
+    if kmax is None:
+        kmax = n
+
+    # k cycles from 1 to n
+    for k in range(1, kmax+1):
+
+        x = A[k-1: m+1, k-1]
+
+        # initialise e_1 of length m-k+1
+        e_1 = np.eye(np.size(x,0))[:,0]
+
+
+        # householder algorithm
+        v_k = np.sign(x[0]) * np.linalg.norm(x) * e_1 + x
+        v_k = v_k / np.linalg.norm(v_k)
+
+        A[k-1: m+1, k-1: kmax] = A[k-1: m+1, k-1: kmax]  - 2 * np.outer(v_k, v_k) @ A[k-1: m+1, k-1: kmax]
+
+        # for case when kmax =/= n, compute Q*b in place of b
+        A[:, kmax:][k-1:,:] = A[:, kmax:][k-1:,:] - 2 * np.outer(v_k, v_k) @ A[:, kmax:][k-1:,:]
+    return A
+
+def householder_ls_recycle(A, b, Qh = None, Rh = None):
+    """
+    Given a real mxn matrix A and an m dimensional vector b, find the
+    least squares solution to Ax = b.
+
+    :param A: an mxn-dimensional numpy array
+    :param b: an m-dimensional numpy array
+
+    :return x: an n-dimensional numpy array
+    """
+    mm, nn = AA.shape
+    
+    ### construct augmented matrix Ahat
+    ###A_hat = np.hstack((A, b[:, None]))
+
+    # use housholder to computer Q and R
+    if Qh is not None:
+        A_hat = np.hstack((AA, bb[:, None]))
+
+        # apply the last householder reflector to the augmented matrix
+        x = A_hat[mm-1: mm+1, mm-1]
+        e_1 = np.eye(np.size(x,0))[:,0]
+        v_k = np.sign(x[0]) * np.linalg.norm(x) * e_1 + x
+        v_k = v_k / np.linalg.norm(v_k)
+        A_hat[mm-1: mm+1, mm-1: mm] = A_hat[mm-1: mm+1, mm-1: mm]  - 2 * np.outer(v_k, v_k) @ A_hat[mm-1: mm+1, mm-1: mm]
+        A_hat_householder = A_hat
+    else:
+        # construct augmented matrix Ahat
+        A_hat = np.hstack((AA, bb[:, None]))
+
+        A_hat_householder = householder(A_hat, nn)
+
+    # extract R and Q^* b using slice notation
+    R = A_hat_householder[:nn, :nn]
+    Q_star_b = A_hat_householder[:nn, nn]
+
+    Q = Q_star_b.T.conjugate()
+    R = R @ Rh
+
+    # solve the upper triangular system  Rx= Q^* b
+    x = scipy.linalg.solve_triangular(R[:, :mm], Q_star_b)
+
+    return x, Q, R
+
+def householder_full_qr(A):
+    """
+    Given a real mxn matrix A, use the Householder transformation to find
+    the full QR factorisation of A.
+    :param A: an mxn-dimensional numpy array
+    :return Q: an mxm-dimensional numpy array
+    :return R: an mxn-dimensional numpy array
+    """
+    m, n= A.shape
+    
+    # create an mxm identity matrix
+    I = np.eye(m)
+
+    # construct extended array Ahat
+    A_hat = np.hstack((A, I))
+
+    # use housholder to computer Q and R
+    A_hat_householder = householder(A_hat, n)
+
+    # extract Q_star and R using slice notation
+    R = A_hat_householder[:, :n]
+    Q_star = A_hat_householder[:, n:]
+
+    return Q_star.T, R
 
 def GMRES(A, b, maxit, tol, x0=None, return_residual_norms=False,
           return_residuals=False):
@@ -71,6 +177,10 @@ def GMRES(A, b, maxit, tol, x0=None, return_residual_norms=False,
     # list storing the residuals at each iteration
     r = []
 
+    ###
+    Qh = None
+    Rh = None
+
     Q = np.zeros((m, maxit+1))
     H = np.zeros((maxit+1,maxit))
     Q[:,0] = x / np.linalg.norm(x)
@@ -91,8 +201,12 @@ def GMRES(A, b, maxit, tol, x0=None, return_residual_norms=False,
         # create basis vector e1
         e1 = np.eye((k+1)+1)[:,0]
         
+        # Householder QR decomposiiton of Hk
+        Qh, Rh = householder_full_qr(Hk)
+        Qh_reduced = Qh[:, :k+1]
+        Rh_reduced = Rh[:k+1, :k+1]
         # Find y by least squares
-        y = householder_ls(Hk, np.linalg.norm(b) * e1)
+        y = scipy.linalg.solve_triangular(Rh_reduced, Qh_reduced.conjugate().T @ (np.linalg.norm(b) * e1))
         
         x = Qk @ y
 
